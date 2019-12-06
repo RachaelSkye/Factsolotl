a9bc65d25006fdde7b2bec7fc91bb3af178d9f0a
Rachael Bouissey <rbouissey@gmail.com>
2019-12-06 13:36:41 -0800

    add design template

--- src/containers/temp-data.js
+++ src/containers/temp-data.js
@@ -1,23 +1,31 @@
 
  export const pws = {
-    "id": "",
-    "name": "",
+    "id": "1",
+    "name": "Public Water System",
     "zip": [
-      "",
-      "",
-      ""
+      "00000",
+      "11111",
+      "22222"
     ],
-    "ccr": {
+    "ccr-2019": {
       "analytes": {
         "lead": {
-          "concentration": 0.001,
+          "range": '<0.001 - 0.008',
+          "avg-conc": 0.004,
           "units": "ppb",
-          "MCL": ""
+          "MCL": 0.01,
+          "MCL-units": "ppb",
+          "MRL": 0.001,
+          "MRL-units": "ppb"
         },
         "copper": {
-          "concentration": 0.001,
+          "range": '5.0 - 20.0',
+          "avg-conc": 10.0,
           "units": "ppb",
-          "MCL": ""
+          "MCL": 0,
+          "MCL-units": "ppb",
+          "MRL": 0,
+          "MRL-units": "ppb"
         }
       }
     }
