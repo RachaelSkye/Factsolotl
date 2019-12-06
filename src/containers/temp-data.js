600f9de36dda7d0700a809c83660b1250ace05d5
Rachael Bouissey <rbouissey@gmail.com>
2019-12-06 12:50:55 -0800

    created temp data template

---
+++ src/containers/temp-data.js
@@ -0,0 +1,24 @@
+
+ export const pws = {
+    "id": "",
+    "name": "",
+    "zip": [
+      "",
+      "",
+      ""
+    ],
+    "ccr": {
+      "analytes": {
+        "lead": {
+          "concentration": 0.001,
+          "units": "ppb",
+          "MCL": ""
+        },
+        "copper": {
+          "concentration": 0.001,
+          "units": "ppb",
+          "MCL": ""
+        }
+      }
+    }
+ }
