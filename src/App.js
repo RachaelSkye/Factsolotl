ae8172d9b7315f1bb98bd102134a2dbd77d409eb
Rachael Bouissey <rbouissey@gmail.com>
2019-12-07 22:05:06 -0800

    add animated bubbles

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import './App.css';
 import Search from './containers/search/search';
 import factsolotl from './img/factsolotl.png';
 
+
 function App() {
   return (
     <div className="App">
-      <div className="bubbles">
-
-        <img src={factsolotl} alt="A pink cartoon axolotl"/>
-
+       <div className="sliding-background"></div>
+        <a href="https://www.scientificamerican.com/article/biologys-beloved-amphibian-the-axolotl-is-racing-toward-extinction1/" target="_blank"><img className="App-logo" src={factsolotl} alt="A pink cartoon axolotl"/></a>
         <header className="App-header">
           <Search />
         </header>
-      </div>
     </div>
   );
 }
