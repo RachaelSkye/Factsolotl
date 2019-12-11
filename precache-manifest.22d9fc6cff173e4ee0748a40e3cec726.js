e4f5fd14c4a54d0ffa697fd8f86d31a427b5d134
Rachael Bouissey <rbouissey@gmail.com>
2019-12-10 21:54:53 -0800

    Updates

---
+++ precache-manifest.22d9fc6cff173e4ee0748a40e3cec726.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca633cbba46a357fa41ddc55a2c37039",
+    "url": "/Factsolotl/index.html"
+  },
+  {
+    "revision": "a8e856993b7e09e2f865",
+    "url": "/Factsolotl/static/css/main.039cf4d4.chunk.css"
+  },
+  {
+    "revision": "6636a63ff37d0b0a655c",
+    "url": "/Factsolotl/static/js/2.373af438.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Factsolotl/static/js/2.373af438.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8e856993b7e09e2f865",
+    "url": "/Factsolotl/static/js/main.1878104a.chunk.js"
+  },
+  {
+    "revision": "dd9ef4beb90f189ce8a6",
+    "url": "/Factsolotl/static/js/runtime-main.ecfc0a59.js"
+  },
+  {
+    "revision": "a8e5380043030175fa17cdc346fbdca9",
+    "url": "/Factsolotl/static/media/factsolotl.a8e53800.png"
+  },
+  {
+    "revision": "46b1b552045e40fe9ac027363b321c0e",
+    "url": "/Factsolotl/static/media/factsolotlBG.46b1b552.png"
+  },
+  {
+    "revision": "77861a73a81393e5ccf840fae693bf7a",
+    "url": "/Factsolotl/static/media/factsolotlBubbles.77861a73.png"
+  }
+]);
