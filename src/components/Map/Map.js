2eb4c670d2b29bd2342e6c483d08b35f3902011b
Rachael Bouissey <rbouissey@gmail.com>
2019-12-18 09:39:40 -0800

    splash style

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -4,8 +4,6 @@ import * as classes from "./Map.module.css";
 const Map = props => {
   return (
     <div>
-      <p className={classes.mapLabel}>Exceedences by county:</p>
-
       <div id="chart_div" className={classes.map}></div>
     </div>
   );
--- src/components/Splash/Splash.js
+++ src/components/Splash/Splash.js
@@ -1,22 +1,20 @@
 import React from "react";
 import * as classes from "./Splash.module.css";
-import './Splash.module.css';
+import "./Splash.module.css";
 import Map from "../Map/Map";
-import DontPanic from '../DontPanic/DontPanic';
+import DontPanic from "../DontPanic/DontPanic";
 
 const Splash = props => {
   return (
     <div className={classes.splash}>
-     
-
       <div className={classes.info}>
-        <h5>Factsolotl</h5>
-        <p>The water quality fact finder.</p>
-
-        <p>
+        <h2 className={classes.title}>Factsolotl</h2>
+        <p className={classes.content}>The water quality fact finder.</p>
+        <h4 className={classes.content2}>
           Use Factsolotl to search for lead results from tap water samples taken
           at public schools across California from 2017 to 2019.
-        </p>
+        </h4>
+
         <button
           type="click"
           className="btn green"
@@ -26,12 +24,21 @@ const Splash = props => {
         </button>
       </div>
       <div className={classes.fortyTwo}>
-      <h1>Don't Panic</h1>
+        <h1>Don't Panic</h1>
 
         <p>Stay informed.</p>
       </div>
-        <Map />
-      
+
+      <div className={classes.legend}>
+        <label>
+          The map below shows the number of samples, per county, had an
+          exceedance. An exceedance is a lead result greater than 15ppb.
+        </label>
+        <br />
+        <br />
+        <label>Hover over a marker to see the county and numbers.</label>
+      </div>
+      <Map />
     </div>
   );
 };
